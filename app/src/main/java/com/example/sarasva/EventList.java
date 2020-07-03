280368fcbd7e8746a10600b229f8836acdca0c66
mtpra <mtpranshu2001@gmail.com>
2020-07-03 23:43:40 +0530

    Sarasva App Carousel View Page added sucessful

--- app/src/main/java/com/example/sarasva/EventList.java
+++ app/src/main/java/com/example/sarasva/EventList.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class EventList extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private Button tedX, mun,kavi,cognoscentia,litathon,brainbrawl,feathers;
 
     private int[] images = {R.drawable.tedx,
                             R.drawable.mun,
@@ -19,23 +26,46 @@ public class EventList extends AppCompatActivity {
                             R.drawable.litathon,
                             R.drawable.brainbrawl};
 
-    LinearLayoutManager linearLayoutManager;
 
-    private EventAdaptor adaptor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
 
-        recyclerView = findViewById(R.id.recyclerEvents);
-        linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        adaptor = new EventAdaptor(EventList.this,images);
-        recyclerView.setAdapter(adaptor);
-        adaptor.notifyDataSetChanged();
+        CarouselView carouselView = findViewById(R.id.cv);
+        carouselView.setPageCount(images.length);
+        carouselView.setImageListener(imageListener);
 
+        setupButtons();
+
+        tedX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventList.this,TedxActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(images[position]);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+    };
+
+    public void setupButtons()
+    {
+        tedX = findViewById(R.id.btntedX);
+        mun = findViewById(R.id.btnmun);
+        kavi = findViewById(R.id.btnkavi);
+        cognoscentia = findViewById(R.id.btncognosentia);
+        litathon = findViewById(R.id.btnlitathon);
+        brainbrawl = findViewById(R.id.btnbrainbrawl);
+        feathers = findViewById(R.id.btnfeathers);
     }
 }
